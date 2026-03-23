8ed887b7c0c887a515c7ae257d4426f67390b837
SimonBara <simobara@hotmail.it>
2026-03-23 06:51:06 -0500

    favicon updated

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -542,7 +542,7 @@ export default function MenuPage() {
                 <div className="flex justify-start -ml-2 md:-ml-6 ">
                   <div className="relative w-[140%] md:w-[600px] h-[185px] md:h-[240px]">
                     <Image
-                      src="/imag/menu5.png"
+                      src="/imag/menu5555.png"
                       alt="Slice menu"
                       fill
                       className="object-contain max-md:scale-y-[1.0] md:scale-[0.95]"
